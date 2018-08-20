6d2a27a4ba42f753417a373ba42453bd3aeb70bd
Daniel Tull <dt@danieltull.co.uk>
2018-08-20 12:13:38 +0100

    Add logging for size transition

--- Collection View Layout Bounds Change Test/CollectionViewController.swift
+++ Collection View Layout Bounds Change Test/CollectionViewController.swift
@@ -1,7 +1,20 @@
 
 import UIKit
 
-final class CollectionViewController: UICollectionViewController {}
+final class CollectionViewController: UICollectionViewController {
+
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+
+		super.viewWillTransition(to: size, with: coordinator)
+
+		guard let collectionView = collectionView else { fatalError() }
+
+		print("viewWillTransition(to size:with coordinator:) START collection view bounds: \(collectionView.bounds))")
+		coordinator.animate(alongsideTransition: nil) { _ in
+			print("viewWillTransition(to size:with coordinator:) END collection view bounds: \(collectionView.bounds))")
+		}
+	}
+}
 
 // MARK: - UICollectionViewDataSource
 
--- Collection View Layout Bounds Change Test/ViewController.swift
+++ Collection View Layout Bounds Change Test/ViewController.swift
@@ -1,7 +1,22 @@
 
 import UIKit
 
-final class ViewController: UIViewController {}
+final class ViewController: UIViewController {
+
+	@IBOutlet private var collectionView: UICollectionView?
+
+	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+
+		super.viewWillTransition(to: size, with: coordinator)
+
+		guard let collectionView = collectionView else { fatalError() }
+
+		print("viewWillTransition(to size:with coordinator:) START collection view bounds: \(collectionView.bounds))")
+		coordinator.animate(alongsideTransition: nil) { _ in
+			print("viewWillTransition(to size:with coordinator:) END collection view bounds: \(collectionView.bounds))")
+		}
+	}
+}
 
 // MARK: - UICollectionViewDataSource
 
